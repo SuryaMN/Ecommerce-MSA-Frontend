631065b5adf5d04d15dcf26a39ca81bd3151cd06
SuryaMN <suryamnnitk@gmail.com>
2022-08-29 20:33:00 +0530

    Debugged profile.js

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import jwt_decode from "jwt-decode";
-import axios from "axios";
+import client from "../utils/client";
 
 function Profile() {
   const [username, setUsername] = useState("");
